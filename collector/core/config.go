b5ea52ff51188d1206c076df008f0e673c89a3d5
Ben Grewell <bgrewell@gmail.com>
2020-11-30 20:53:09 -0800

    adding collector service

---
+++ collector/core/config.go
@@ -0,0 +1,34 @@
+package core
+
+import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+
+type SensorConfig struct {
+	Name      string   `json:"name" yaml:"name" xml:"name"`
+	Host      string   `json:"host" yaml:"host" xml:"host"`
+	Latitude  float64  `json:"latitude" yaml:"latitude" xml:"latitude"`
+	Longitude float64  `json:"longitude" yaml:"longitude" xml:"longitude"`
+	Tags      []string `json:"tags" yaml:"tags" xml:"tags"`
+}
+
+type Configuration struct {
+	ServerUrl   string          `json:"server_url" yaml:"server_url" xml:"server_url"`
+	ServerPort  int             `json:"server_port" yaml:"server_port" xml:"server_port"`
+	SSLCertFile string          `json:"ssl_cert_file" yaml:"ssl_cert_file" xml:"ssl_cert_file"`
+	SSLKeyFile  string          `json:"ssl_key_file" yaml:"ssl_key_file" xml:"ssl_key_file"`
+	Sensors     []*SensorConfig `json:"sensors" yaml:"sensors" xml:"sensors"`
+}
+
+func LoadConfig(filename string) (config *Configuration, err error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	c := &Configuration{}
+	if err = yaml.Unmarshal(data, c); err != nil {
+		return nil, err
+	}
+	return c, nil
+}
--- collector/main.go
+++ collector/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+    "github.com/BGrewell/perspective/collector/core"
+    log "github.com/sirupsen/logrus"
+)
+
+/*
+Collector - This is the component of perspective that polls the REST endpoints and
+aggregates all of the event records into a central store. It also provides the endpoints
+that the dashboard connects to in order to poll the event information
+ */
+
+
+
 func main() {
-	
+
+    // Parse the configuration file
+    config, err := core.LoadConfig("/etc/collectord/config.yaml")
+    if err != nil {
+        log.Fatalf("failed to parses config: %v", err)
+    }
+
+    // Start a go-routine to poll each sensor
+    for idx, sensor := range config.Sensors {
+        log.Printf("sensor %d: %s", idx, sensor.Name)
+    }
+
+    // Start web server
 }
